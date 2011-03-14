560545276965ef02c8096ecda7b4f9349548941f
Steve Ivy <steve@wallrazer.com>
2011-03-14 13:45:04 -0700

    fix not found exception

--- typepadapp/admin.py
+++ typepadapp/admin.py
@@ -190,7 +190,7 @@ def delete_subscription(**kwargs):
             typepad.client.complete_batch()
 
             messages.add_message(request, messages.INFO, "Subscription deleted from Typepad")
-        except Exception:
+        except typepad.ExternalFeedSubscription.NotFound:
             messages.add_message(request, messages.ERROR, "Could not fund subscription with ID: %s" % url_id)
             
 admin.site.register(Subscription, SubscriptionAdmin)
